b71b41011f003ef9be6154167f92c447f78b4224
deadlylaid <deadlylaid@gmail.com>
2017-02-07 14:08:38 +0900

    home view FBV -> CBV

--- lucifer/lucifer/views/home.py
+++ lucifer/lucifer/views/home.py
@@ -1,13 +1,7 @@
-from django.views.generic import View
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
 
-class Home(View):
+class Home(TemplateView):
 
-    def get(self, request):
-
-        return render(
-                request,
-                "home.html",
-                context={},
-                )
+    template_name = "home.html"
