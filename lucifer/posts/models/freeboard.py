fc78974e2b159b3d167da3a9a0f81bc4d6721915
deadlylaid <deadlylaid@gmail.com>
2017-02-09 15:34:02 +0900

    게시 날짜, 수정 날짜 속성 추가

--- lucifer/posts/models/freeboard.py
+++ lucifer/posts/models/freeboard.py
@@ -15,3 +15,11 @@ class FreeBoard(models.Model):
     content = models.TextField(
             max_length=255,
             )
+
+    created_at = models.DateTimeField(
+            auto_now_add=True,
+            )
+
+    updated_at = models.DateTimeField(
+            auto_now=True,
+            )
