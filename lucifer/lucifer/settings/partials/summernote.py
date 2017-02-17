5741a76e4c804535589b408233f6bf49fcec3c0f
deadlylaid <deadlylaid@gmail.com>
2017-02-17 20:29:14 +0900

    ㅈdjango-summernote 기능 수정 이미지 업로드 기능 추가

--- lucifer/lucifer/settings/partials/summernote.py
+++ lucifer/lucifer/settings/partials/summernote.py
@@ -15,5 +15,10 @@ SUMMERNOTE_CONFIG = {
         'width': '90%',
         'attachment_upload_to': uploaded_filepath,
         'attachment_filesize_limit': 1024 * 1024,
-
-        }
+        'toolbar': [
+            ['style', ['style']],
+            ['insert', ['link', 'picture']],
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['para', ['ul', 'ol', 'height']],
+        ],
+}
--- lucifer/lucifer/urls.py
+++ lucifer/lucifer/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from .views import Home
 from users.views import *
@@ -32,4 +34,4 @@ urlpatterns = [
     url(r'^community/freeboard/(?P<pk>\d+)/$', DetailFreeBoard.as_view(), name='free_detail'),
     url(r'^community/freeboard/$', ListFreeBoard.as_view(), name='free_list'),
     url(r'^summernote/', include('django_summernote.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
