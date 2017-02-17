0460d37bf9b22041a5e72ce55b7a0c3a74b3be7d
deadlylaid <deadlylaid@gmail.com>
2017-02-17 23:57:03 +0900

    댓글 작성 회원가입된 유저만 가능하도록 수정

--- lucifer/posts/views/answer.py
+++ lucifer/posts/views/answer.py
@@ -1,12 +1,16 @@
 from django.views.generic import CreateView
+from django.views.decorators.http import require_POST
+from django.utils.decorators import method_decorator
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from posts.models import Answer, FreeBoard
 
 
-class CreateAnswer(CreateView):
+@method_decorator(require_POST, name='dispatch')
+class CreateAnswer(LoginRequiredMixin, CreateView):
 
+    login_url = '/login/'
     model = Answer
 
     fields = [
