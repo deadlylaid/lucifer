58ed8cf06fbec913bbb816cc3cffa7c854eeb289
deadlylaid <deadlylaid@gmail.com>
2017-02-20 13:32:53 +0900

    STATIC_ROOT, MEDIA_ROOT 경로 추가

--- lucifer/lucifer/settings/partials/static.py
+++ lucifer/lucifer/settings/partials/static.py
@@ -1,5 +1,10 @@
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+import os
+from .base import BASE_DIR, BASE_ROOT_DIR
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_ROOT_DIR, "dist", "static")
+MEDIA_ROOT = os.path.join(BASE_ROOT_DIR, "dist", "media")
