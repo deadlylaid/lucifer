76450a62c85d1614eaf9fdebee2a5c39cbd1d71e
deadlylaid <deadlylaid@gmail.com>
2017-02-26 13:56:47 +0900

    캐릭터 API뷰를 이용해 캐릭터 정보 출력 character_serializer + status_serializer

--- lucifer/game/characters/api/serializers/character_serializer.py
+++ lucifer/game/characters/api/serializers/character_serializer.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 from game.characters.models import Character
+from .status_serializer import StatusSerializer
 
 
 class CharacterSerializer(serializers.ModelSerializer):
 
+    status = StatusSerializer(read_only=True)
+
     class Meta:
         model = Character
-        fields = '__all__'
+        fields = (
+                'nickname',
+                'level',
+                'job',
+                'complete_quest_set',
+
+                'status',
+                )
