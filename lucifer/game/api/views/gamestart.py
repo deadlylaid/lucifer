bf34b6219fc22e27adf2c1d188e555a952150c00
deadlylaid <deadlylaid@gmail.com>
2017-03-03 15:00:11 +0900

    GameStartAPIView에 serializers 추가 NPC, Quest, Skill, Stage 추가

--- lucifer/game/api/views/gamestart.py
+++ lucifer/game/api/views/gamestart.py
@@ -9,6 +9,18 @@ from game.items.api.serializers import ItemSerializer
 from game.monsters.models import Monster
 from game.monsters.api.serializer import MonsterSerializer
 
+from game.npc.models import NPC
+from game.npc.api.serializers import NPCSerializer
+
+from game.quests.models import Quest
+from game.quests.api.serializer import QuestSerializer
+
+from game.skills.models import Skill
+from game.skills.api.serializers import SkillSerializer
+
+from game.stages.models import Stage
+from game.stages.api.serializers import StageSerializer
+
 
 class GameStartAPIView(MultipleModelAPIView):
 
@@ -19,6 +31,10 @@ class GameStartAPIView(MultipleModelAPIView):
                 (Character.objects.filter(user=logined_user), CharacterSerializer),
                 (Monster.objects.all(), MonsterSerializer),
                 (Item.objects.all(), ItemSerializer),
+                (NPC.objects.all(), NPCSerializer),
+                (Quest.objects.all(), QuestSerializer),
+                (Skill.objects.all(), SkillSerializer),
+                (Stage.objects.all(), StageSerializer),
                 )
 
         return queryList
