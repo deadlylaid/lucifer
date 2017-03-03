9cedf4ece78ac3ef7fdf9cb988102678ca9ca58f
deadlylaid <deadlylaid@gmail.com>
2017-03-03 14:05:02 +0900

    게임 시작 클릭시, Character, Monster, Item Api가 호출되도록 수정 drf_multiple_model을 이용해서 구현

--- lucifer/game/api/views/gamestart.py
+++ lucifer/game/api/views/gamestart.py
@@ -1,2 +1,24 @@
-from rest_framework.generics import ListAPIView
+from drf_multiple_model.views import MultipleModelAPIView
+
 from game.characters.models import Character
+from game.characters.api.serializers import CharacterSerializer
+
+from game.items.models import Item
+from game.items.api.serializers import ItemSerializer
+
+from game.monsters.models import Monster
+from game.monsters.api.serializer import MonsterSerializer
+
+
+class GameStartAPIView(MultipleModelAPIView):
+
+    def get_queryList(self):
+        logined_user = self.request.user
+
+        queryList = (
+                (Character.objects.filter(user=logined_user), CharacterSerializer),
+                (Monster.objects.all(), MonsterSerializer),
+                (Item.objects.all(), ItemSerializer),
+                )
+
+        return queryList
