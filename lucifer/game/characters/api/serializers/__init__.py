8ea55eb5e81d2917d6142f6b16272228c9b68e49
deadlylaid <deadlylaid@gmail.com>
2017-03-05 23:01:15 +0900

    character_serializer에 quickslot api 추가 호출

--- lucifer/game/characters/api/serializers/__init__.py
+++ lucifer/game/characters/api/serializers/__init__.py
@@ -2,3 +2,4 @@ from .character_serializer import CharacterSerializer
 from .status_serializer import StatusSerializer
 from .inventory_serializer import InventorySerializer
 from .learnedskill_serializer import LearnedSkillSerializer
+from .quickslot_serializer import QuickSlotSerializer
--- lucifer/game/characters/api/serializers/character_serializer.py
+++ lucifer/game/characters/api/serializers/character_serializer.py
@@ -3,6 +3,7 @@ from game.characters.models import Character
 from .status_serializer import StatusSerializer
 from .inventory_serializer import InventorySerializer
 from .learnedskill_serializer import LearnedSkillSerializer
+from .quickslot_serializer import QuickSlotSerializer
 
 
 class CharacterSerializer(serializers.ModelSerializer):
@@ -10,6 +11,7 @@ class CharacterSerializer(serializers.ModelSerializer):
     status = StatusSerializer()
     inventory_set = InventorySerializer(many=True)
     learnedskill_set = LearnedSkillSerializer(many=True)
+    quickslot_set = QuickSlotSerializer(many=True)
 
     class Meta:
         model = Character
@@ -22,4 +24,5 @@ class CharacterSerializer(serializers.ModelSerializer):
                 'status',
                 'inventory_set',
                 'learnedskill_set',
+                'quickslot_set',
                 )
