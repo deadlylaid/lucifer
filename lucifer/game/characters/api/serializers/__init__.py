4246e71abc1501ddb3c7f2c21d4aee61ac06da53
deadlylaid <deadlylaid@gmail.com>
2017-03-05 23:27:04 +0900

    Equipment api 추가

--- lucifer/game/characters/api/serializers/__init__.py
+++ lucifer/game/characters/api/serializers/__init__.py
@@ -3,3 +3,4 @@ from .status_serializer import StatusSerializer
 from .inventory_serializer import InventorySerializer
 from .learnedskill_serializer import LearnedSkillSerializer
 from .quickslot_serializer import QuickSlotSerializer
+from .equipment_serializer import EquipmentSerializer
--- lucifer/game/characters/api/serializers/character_serializer.py
+++ lucifer/game/characters/api/serializers/character_serializer.py
@@ -4,6 +4,7 @@ from .status_serializer import StatusSerializer
 from .inventory_serializer import InventorySerializer
 from .learnedskill_serializer import LearnedSkillSerializer
 from .quickslot_serializer import QuickSlotSerializer
+from .equipment_serializer import EquipmentSerializer
 
 
 class CharacterSerializer(serializers.ModelSerializer):
@@ -12,6 +13,7 @@ class CharacterSerializer(serializers.ModelSerializer):
     inventory_set = InventorySerializer(many=True)
     learnedskill_set = LearnedSkillSerializer(many=True)
     quickslot_set = QuickSlotSerializer(many=True)
+    equipment_set = EquipmentSerializer(many=True)
 
     class Meta:
         model = Character
@@ -25,4 +27,5 @@ class CharacterSerializer(serializers.ModelSerializer):
                 'inventory_set',
                 'learnedskill_set',
                 'quickslot_set',
+                'equipment_set',
                 )
