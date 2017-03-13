1a19915fd202c6be1ac994d6648a58cfc1b8ab68
deadlylaid <deadlylaid@gmail.com>
2017-03-13 23:54:24 +0900

    notice 뷰 구현 완료

--- lucifer/posts/views/notice.py
+++ lucifer/posts/views/notice.py
@@ -1,12 +1,11 @@
-from django.views.generic import View
+from django.views.generic import ListView
 from django.shortcuts import render
 
+from posts.models import Notice
 
-class ListNotice(View):
 
-    def get(self, request):
-        return render(
-                request,
-                "posts/notice_list.html",
-                context={},
-                )
+class ListNotice(ListView):
+
+    model = Notice
+    template_name = "posts/notice_list.html"
+    context_object_name = "notices"
