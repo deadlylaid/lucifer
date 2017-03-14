1570ebcb642acf278e87c027b75616d0e4dfa0e2
OverFace <ygchoi2001@naver.com>
2017-03-15 05:08:30 +0900

    MainGame.js : sprite 바뀜, 마우스 이벤트

--- lucifer/lucifer/static/js/game/MainGame.js
+++ lucifer/lucifer/static/js/game/MainGame.js
@@ -18,8 +18,9 @@ function main(evt){
 
 	//--Sprite Node Create
 	//****************************************************************************************************
+	//--Stand Sprite
 	var SpriteNode = new Pig2d.node();
-	var model 	   = new Pig2d.SpriteModel({
+	var Standmodel = new Pig2d.SpriteModel({
 		data : 
 		{
 			"name" : "Bavarian_Stand",
@@ -99,27 +100,220 @@ function main(evt){
 				},
 			]
 		},
-		imgObj : textures['Stand.png']
+		imgObj : textures['stand/Stand.png']
+	});	
+
+	//--Walk Sprite	
+	var Walk_SpriteNode = new Pig2d.node();	
+	var Walkmodel = new Pig2d.SpriteModel({
+		data : 
+		{
+			"name" : "Bavarian_Walk",
+			"frames" : 
+			[
+				{"sheets" : 	//0번재
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": 0, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//1번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//2번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -400, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//3번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 3, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//4번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 4, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//5번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 5, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//6번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 6, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//7번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 7, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+			]
+		},
+		imgObj : textures['walk/Walk.png']
 	});
 
-	//--Sprite set / get
-	SpriteNode.set( {model: model} );
-	SpriteNode.get('model').setupAnimation();
-
+	//--Sprite set	
+	SpriteNode.set( {model: Standmodel} );		
+	Walk_SpriteNode.set( {model: Walkmodel} );		
+	
 	//--Play, stop, ready
+	SpriteNode.get('model').setupAnimation();
 	SpriteNode.get('model').set('AnimationStatus', 'play');
-	SpriteNode.get('model').set('isAnimationLoop', true);
+	SpriteNode.get('model').set('isAnimationLoop', true);	
 
-	var sprite_node = Pig2d.util.createDummy();
-	//sprite_node.get('model').set('flipY', true);	//좌우 뒤집기
+	Walk_SpriteNode.get('model').setupAnimation();
+	Walk_SpriteNode.get('model').show(false);
+
+	var sprite_node = Pig2d.util.createDummy();	
 	sprite_node.get('model').setPosition(640, 400);
 	sprite_node.add(SpriteNode);
-
+	sprite_node.add(Walk_SpriteNode);
+	
 	//SceneMgr Add
-	SceneMgr.add(sprite_node);
+	SceneMgr.add(sprite_node);		
+
+	/*
  	sprite_node.get('model').setupTransition({
  		TransitionEndCallBack : function(){}
+ 	}); 	 
+ 	*/
+
+ 	//--Animation Data Setting
+ 	/*
+ 	var Animation_Data = {
+ 		"frames" : 
+			[
+				{"sheets" : 	//0번재
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": 0, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//1번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//2번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -400, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//3번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 3, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//4번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 4, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//5번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 5, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//6번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 6, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+				{"sheets" : 	//7번째
+					[ 
+					  {
+						"width": 200, "height": 200, 
+					    "centerOffset" : {"x" : -100, "y" : -100},
+					    "bp_x": -200 * 7, "bp_y": 0
+					  } 
+					], "delay" : "100" 
+				},
+			] 		
+ 	}
+
+ 	//--Sprite Node Create
+ 	var Node_Stand = Pig2d.util.createSprite({
+ 		texture : textures['stand/Stand.png'],
+ 		animation : Animation_Data
+ 	}); 	
+
+ 	var Node_Walk = Pig2d.util.createSprite({
+ 		texture : textures['walk/Walk.png'],
+ 		animation : Animation_Data
  	});
+
+ 	//Node_Stand.get('model').setFrame(0);
+ 	//Node_Stand.get('model').set('AnimationStatus', 'play');
+ 	Node_Stand.get('model').setPosition(640, 400);
+ 	Node_Walk.get('model').setPosition(640, 400);
+
+ 	//--SceneMgr add
+ 	SceneMgr.add(Node_Stand); 	
+ 	//SceneMgr.add(Node_Walk);
+ 	*/
  	//****************************************************************************************************
 
  	//--Controller Setting
@@ -129,19 +323,61 @@ function main(evt){
 
 	//--Function Rogic
 	//****************************************************************************************************
-	//--Player Move(Mouse Click)
+	
+	var MouseControler = new Pig2d.util.controller.MouseSpot({
+		listener_element : document,
+		node : sprite_node,
+		speed : 150,
+		setupCallBack : function(){
+		},
+		endCallBack : function(){
+			/*
+			Node_Stand.get('model').setupAnimation({
+				startFrame: 0,
+				endFrame: 7,
+				isAnimationLoop: true,
+				AnimationStatus: 'play'
+			});
+			*/
+			SpriteNode.get('model').show(true);
+			SpriteNode.get('model').setupAnimation();
+			SpriteNode.get('model').set('AnimationStatus', 'play');
+			SpriteNode.get('model').set('isAnimationLoop', true);
+
+			Walk_SpriteNode.get('model').show(false);	
+		},
+		startCallBack : function(evt){		
+			/*
+			Node_Walk.get('model').setupAnimation({
+						isAnimationLoop: true,
+						AnimationStatus: 'play'
+			});
+			*/			
+			SpriteNode.get('model').show(false);	
+			Walk_SpriteNode.get('model').show(true);
+			Walk_SpriteNode.get('model').set('AnimationStatus', 'play');
+			Walk_SpriteNode.get('model').set('isAnimationLoop', true);		
+		}
+	});	
+
+	/*
 	document.body.addEventListener('click', function(evt){
+		//Current_Pos & New Pos
 		var Current_Position = sprite_node.get('model').getPosition();
 		var New_Poisiton	 = new gbox3d.core.Vect2d(evt.layerX, evt.layerY);
 
 		//Current Position 과 New Position 간의 거리.
 		var Distance = (New_Poisiton.clone()).subToThis(Current_Position).getDistance();
 
+		//Srpite Change
+
+		//Dummy Trans
 		sprite_node.get('model').transition({
 			position: New_Poisiton,
 			time : Distance / 150	//1초에 150 픽셀 만큼 이동.
-		});
+		});		
 	});
+	*/
 	//****************************************************************************************************
 	
 	//--Game Loop
@@ -171,9 +407,10 @@ function main(evt){
 }
 
 Pig2d.util.SetupAsset({
-	asset_path : "../../static/images/game/Player/Bavarian/stand/",
+	asset_path : "../../static/images/game/Player/Bavarian/",
 	img_files : [
-		"Stand.png"
+		"stand/Stand.png",
+		"walk/Walk.png"
 	],
 	OnLoadComplete : main
 });
