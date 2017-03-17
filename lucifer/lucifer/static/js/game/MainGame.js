8446f5d8ac08af9e3106b37e5b7865f18b086452
OverFace <ygchoi2001@naver.com>
2017-03-17 11:41:39 +0900

    MainGame.js : 방향까지 구함

--- lucifer/lucifer/static/js/game/MainGame.js
+++ lucifer/lucifer/static/js/game/MainGame.js
@@ -7,8 +7,13 @@
 * **************************/
 //## Main Function
 function main(evt){
+	//Game Extern Value
+	//*************************************************************************
+	var LookDir = new gbox3d.core.Vect3d(1, 0, 0);
+
 	//--texture
 	var textures = evt.textures;
+	//*************************************************************************
 
 	//--Scene Mgr Create
 	var SceneMgr = new Pig2d.SceneManager({
@@ -19,7 +24,14 @@ function main(evt){
 	//--Sprite Node Create
 	//****************************************************************************************************
 	//--Stand Sprite
-	var SpriteNode = new Pig2d.node();
+	var Direction = 0; 
+	function Change_SpriteNumber(SpriteNumber)
+	{
+		//Sprite bp_y 값 변경
+		Direction = SpriteNumber;
+	}
+
+	var Stand_SpriteNode = new Pig2d.node();
 	var Standmodel = new Pig2d.SpriteModel({
 		data : 
 		{
@@ -31,7 +43,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": 0, "bp_y": 0
+					    "bp_x": 0, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -40,7 +52,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200, "bp_y": 0
+					    "bp_x": -200, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -49,7 +61,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -400, "bp_y": 0
+					    "bp_x": -400, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -58,7 +70,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 3, "bp_y": 0
+					    "bp_x": -200 * 3, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -67,7 +79,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 4, "bp_y": 0
+					    "bp_x": -200 * 4, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -76,7 +88,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 5, "bp_y": 0
+					    "bp_x": -200 * 5, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -85,7 +97,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 6, "bp_y": 0
+					    "bp_x": -200 * 6, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -94,7 +106,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 7, "bp_y": 0
+					    "bp_x": -200 * 7, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -116,7 +128,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": 0, "bp_y": 0
+					    "bp_x": 0, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -125,7 +137,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200, "bp_y": 0
+					    "bp_x": -200, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -134,7 +146,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -400, "bp_y": 0
+					    "bp_x": -400, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -143,7 +155,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 3, "bp_y": 0
+					    "bp_x": -200 * 3, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -152,7 +164,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 4, "bp_y": 0
+					    "bp_x": -200 * 4, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -161,7 +173,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 5, "bp_y": 0
+					    "bp_x": -200 * 5, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -170,7 +182,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 6, "bp_y": 0
+					    "bp_x": -200 * 6, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -179,7 +191,7 @@ function main(evt){
 					  {
 						"width": 200, "height": 200, 
 					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 7, "bp_y": 0
+					    "bp_x": -200 * 7, "bp_y": Direction
 					  } 
 					], "delay" : "100" 
 				},
@@ -189,142 +201,74 @@ function main(evt){
 	});
 
 	//--Sprite set	
-	SpriteNode.set( {model: Standmodel} );		
+	Stand_SpriteNode.set( {model: Standmodel} );		
 	Walk_SpriteNode.set( {model: Walkmodel} );		
 	
 	//--Play, stop, ready
-	SpriteNode.get('model').setupAnimation();
-	SpriteNode.get('model').set('AnimationStatus', 'play');
-	SpriteNode.get('model').set('isAnimationLoop', true);	
+	Stand_SpriteNode.get('model').setupAnimation();
+	Stand_SpriteNode.get('model').set('AnimationStatus', 'play');
+	Stand_SpriteNode.get('model').set('isAnimationLoop', true);	
 
 	Walk_SpriteNode.get('model').setupAnimation();
 	Walk_SpriteNode.get('model').show(false);
 
 	var sprite_node = Pig2d.util.createDummy();	
 	sprite_node.get('model').setPosition(640, 400);
-	sprite_node.add(SpriteNode);
+	sprite_node.add(Stand_SpriteNode);
 	sprite_node.add(Walk_SpriteNode);
 	
 	//SceneMgr Add
 	SceneMgr.add(sprite_node);		
-
-	/*
- 	sprite_node.get('model').setupTransition({
- 		TransitionEndCallBack : function(){}
- 	}); 	 
- 	*/
-
- 	//--Animation Data Setting
- 	/*
- 	var Animation_Data = {
- 		"frames" : 
-			[
-				{"sheets" : 	//0번재
-					[ 
-					  {
-						"width": 200, "height": 200, 
-					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": 0, "bp_y": 0
-					  } 
-					], "delay" : "100" 
-				},
-				{"sheets" : 	//1번째
-					[ 
-					  {
-						"width": 200, "height": 200, 
-					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200, "bp_y": 0
-					  } 
-					], "delay" : "100" 
-				},
-				{"sheets" : 	//2번째
-					[ 
-					  {
-						"width": 200, "height": 200, 
-					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -400, "bp_y": 0
-					  } 
-					], "delay" : "100" 
-				},
-				{"sheets" : 	//3번째
-					[ 
-					  {
-						"width": 200, "height": 200, 
-					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 3, "bp_y": 0
-					  } 
-					], "delay" : "100" 
-				},
-				{"sheets" : 	//4번째
-					[ 
-					  {
-						"width": 200, "height": 200, 
-					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 4, "bp_y": 0
-					  } 
-					], "delay" : "100" 
-				},
-				{"sheets" : 	//5번째
-					[ 
-					  {
-						"width": 200, "height": 200, 
-					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 5, "bp_y": 0
-					  } 
-					], "delay" : "100" 
-				},
-				{"sheets" : 	//6번째
-					[ 
-					  {
-						"width": 200, "height": 200, 
-					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 6, "bp_y": 0
-					  } 
-					], "delay" : "100" 
-				},
-				{"sheets" : 	//7번째
-					[ 
-					  {
-						"width": 200, "height": 200, 
-					    "centerOffset" : {"x" : -100, "y" : -100},
-					    "bp_x": -200 * 7, "bp_y": 0
-					  } 
-					], "delay" : "100" 
-				},
-			] 		
- 	}
-
- 	//--Sprite Node Create
- 	var Node_Stand = Pig2d.util.createSprite({
- 		texture : textures['stand/Stand.png'],
- 		animation : Animation_Data
- 	}); 	
-
- 	var Node_Walk = Pig2d.util.createSprite({
- 		texture : textures['walk/Walk.png'],
- 		animation : Animation_Data
- 	});
  	//****************************************************************************************************
-
- 	//Node_Stand.get('model').setFrame(0);
- 	//Node_Stand.get('model').set('AnimationStatus', 'play');
- 	Node_Stand.get('model').setPosition(640, 400);
- 	Node_Walk.get('model').setPosition(640, 400);
-
- 	//--SceneMgr add
- 	SceneMgr.add(Node_Stand); 	
- 	//SceneMgr.add(Node_Walk);
- 	*/
- 	//****************************************************************************************************
-
-	//--Controller Setting
+ 	//--Controller Setting
 	/*Pig2d.util.setup_pig2dTestController(
 		document,		//Evenet 받을 대상
 		SpriteNode);	//조종할 대상이 되는 객체*/	
 
 	//--Function Rogic
 	//****************************************************************************************************
+	//--Mouse Position & Player Sprite Number Change(방향구함)
+	var GameScreen = document.querySelector(".pig2d-fullscreen");
+	var Angle = 0;
 	
+	GameScreen.addEventListener('click', function(evt){		
+		var MousePos = new gbox3d.core.Vect2d(evt.layerX, evt.layerY);
+		var PlayerPos = sprite_node.get('model').getPosition();
+
+		var PlayerDir = MousePos.sub(PlayerPos);
+		PlayerDir.normalize();
+
+		Angle = PlayerDir.getAngle();
+		var Dir = Angle / 45;
+		//console.log(Dir);
+
+		return Dir;
+
+		/*
+		var MousePos = new gbox3d.core.Vect3d(evt.layerX, evt.layerY, 0);
+		var PlayerPos = sprite_node.get('model').getPosition();				
+		
+		//플레이어가 마우스로 바라보는 방향벡터
+		var PlayerDir = (MousePos.clone()).substract(sprite_node.get('model').getPosition());
+		PlayerDir.normalize();	
+
+		//마우스로 바라보는 방향벡터와 플레이어의 기본 방향 내적.
+		var CosValue = PlayerDir.dotProduct(LookDir);
+		
+		Angle = Math.acos(CosValue);
+		if(sprite_node.get('model').getPosition().y < MousePos.y)
+		{
+			Angle = 2 * Math.PI - Angle;
+		}
+
+		Angle = gbox3d.core.radToDeg(Angle);
+		Direction = Angle / 45;
+
+		console.log(PlayerDir);	
+		*/	
+	});	
+
+	//--Player Move
 	var MouseControler = new Pig2d.util.controller.MouseSpot({
 		listener_element : document,
 		node : sprite_node,
@@ -332,58 +276,23 @@ function main(evt){
 		setupCallBack : function(){
 		},
 		endCallBack : function(){
-			/*
-			Node_Stand.get('model').setupAnimation({
-				startFrame: 0,
-				endFrame: 7,
-				isAnimationLoop: true,
-				AnimationStatus: 'play'
-			});
-			*/
-			SpriteNode.get('model').show(true);
-			SpriteNode.get('model').setupAnimation();
-			SpriteNode.get('model').set('AnimationStatus', 'play');
-			SpriteNode.get('model').set('isAnimationLoop', true);
+			Stand_SpriteNode.get('model').show(true);
+			Stand_SpriteNode.get('model').setupAnimation();
+			Stand_SpriteNode.get('model').set('AnimationStatus', 'play');
+			Stand_SpriteNode.get('model').set('isAnimationLoop', true);
 
-			Walk_SpriteNode.get('model').show(false);	
+			Walk_SpriteNode.get('model').show(false);			
 		},
 		startCallBack : function(evt){		
-			/*
-			Node_Walk.get('model').setupAnimation({
-						isAnimationLoop: true,
-						AnimationStatus: 'play'
-			});
-			*/			
-			SpriteNode.get('model').show(false);	
 			Walk_SpriteNode.get('model').show(true);
 			Walk_SpriteNode.get('model').set('AnimationStatus', 'play');
-			Walk_SpriteNode.get('model').set('isAnimationLoop', true);		
+			Walk_SpriteNode.get('model').set('isAnimationLoop', true);	
+
+			Stand_SpriteNode.get('model').show(false);				
 		}
 	});	
-
-	/*
-	//--Function Rogic
 	//****************************************************************************************************
-	//--Player Move(Mouse Click)
-	document.body.addEventListener('click', function(evt){
-		//Current_Pos & New Pos
-		var Current_Position = sprite_node.get('model').getPosition();
-		var New_Poisiton	 = new gbox3d.core.Vect2d(evt.layerX, evt.layerY);
 
-		//Current Position 과 New Position 간의 거리.
-		var Distance = (New_Poisiton.clone()).subToThis(Current_Position).getDistance();
-
-		//Srpite Change
-
-		//Dummy Trans
-		sprite_node.get('model').transition({
-			position: New_Poisiton,
-			time : Distance / 150	//1초에 150 픽셀 만큼 이동.
-		});		
-	});
-	*/
-	//****************************************************************************************************
-	
 	//--Game Loop
 	//****************************************************************************************************
 	//--Timer Setting & Performance Test Infomation(FPS)
@@ -434,4 +343,4 @@ Pig2d.util.SetupAsset({
 			requestAnimationFrame(loop);
 		}
 	);
-	*/
+*/
