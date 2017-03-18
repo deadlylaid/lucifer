6bad117777616e3388d7b70cd5bb7289b6391675
deadlylaid <deadlylaid@gmail.com>
2017-03-18 14:34:29 +0900

    캐릭터 능력치 변경시 db에 수정사항이 전달되도록 수정 ajax put method를 이용하여 구현

--- lucifer/lucifer/urls.py
+++ lucifer/lucifer/urls.py
@@ -42,8 +42,8 @@ urlpatterns = [
 
     url(r'^api/gamestart/data/$', GameStartAPIView.as_view(), name='api_gamestart'),
 
-    url(r'^api/user/character/status/$', StatusAPIView.as_view(), name='api_status'),
     url(r'^api/user/character/$', CharacterAPIView.as_view(), name='api_character'),
+    url(r'^api/user/character/status/$', StatusAPIView.as_view(), name='api_status'),
 
     url(r'^api/item/list/$', ItemListAPIView.as_view(), name='api_item_list'),
 
