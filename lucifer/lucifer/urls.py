bcde9d8916fb49ff19779829d5daf4e8e2664dad
hoeeon <slhgesa@naver.com>
2017-03-20 02:07:22 +0900

    Modify : Jobintro css

--- lucifer/lucifer/urls.py
+++ lucifer/lucifer/urls.py
@@ -36,10 +36,7 @@ urlpatterns = [
     url(r'^events/$', ListEvent.as_view(), name='event_list'),
     url(r'^qna/$', QnaCreateView.as_view(), name='qna'),
 
-    url(r'^test$', TestView.as_view(), name='test'),
-
     url(r'^jobintro$', JobIntro.as_view(), name='jobintro'),
-
 
     url(r'^check/chracter/$', CheckCharacter.as_view(), name='checkcharacter'),
 
