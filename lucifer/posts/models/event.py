56bd4fef3189c5024c73b745280e8716f9b602fa
deadlylaid <deadlylaid@gmail.com>
2017-03-25 22:05:33 +0900

    deadline_at, event_image, is_end field 추가

--- lucifer/posts/models/event.py
+++ lucifer/posts/models/event.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from datetime import datetime, timedelta
+
+from versatileimagefield.fields import VersatileImageField
 
 
 class Event(models.Model):
@@ -20,6 +23,17 @@ class Event(models.Model):
             auto_now_add=True,
             )
 
-    updated_at = models.DateTimeField(
-            auto_now=True,
+    deadline_at = models.DateTimeField(
+            default=datetime.now()+timedelta(days=30),
             )
+
+    event_image = VersatileImageField(
+            blank=True,
+            null=True,
+            )
+
+    @property
+    def is_end(self):
+        if self.created_at > self.deadline_at:
+            return True
+        return False
