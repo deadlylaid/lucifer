42030c683dfc600fe93750c10b2d34b910664b2e
deadlylaid <deadlylaid@gmail.com>
2017-03-26 19:50:59 +0900

    home.html ranking 연동

--- lucifer/lucifer/views/home.py
+++ lucifer/lucifer/views/home.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 
 from posts.models import Notice, Event, FreeBoard
+from game.characters.models import Character
 
 
 class Home(View):
@@ -32,14 +33,21 @@ class Home(View):
                 20,
                 )
 
+        ranking = Paginator(
+                Character.objects.order_by('status__attack_point'),
+                10,
+                )
+
         showed_notices = notices.page(1)
         showed_events = events.page(1)
         showed_freeboard = freeboard.page(1)
+        showed_ranking = ranking.page(1)
 
         context = {
                 'notices': showed_notices,
                 'events': showed_events,
                 'freeboards': showed_freeboard,
+                'characters': showed_ranking,
                 }
 
         return render(
