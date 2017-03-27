b87c648d5a9150a0be993cb7c7d42b8ac54a275b
deadlylaid <deadlylaid@gmail.com>
2017-03-27 22:21:13 +0900

    screenshot_detail 추가

--- lucifer/posts/views/screenshot.py
+++ lucifer/posts/views/screenshot.py
@@ -1,11 +1,18 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
 from posts.models import ScreenShot
 
 
-class ScreenShot(ListView):
+class ScreenShotListView(ListView):
 
     model = ScreenShot
     template_name = "posts/screenshot.html"
     context_object_name = "screenshots"
+
+
+class ScreenShotDetailView(DetailView):
+
+    model = ScreenShot
+    template_name = "posts/screenshot_detail.html"
+    context_object_name = "screenshot"
