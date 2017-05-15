cc025024a1d2b39985ebaa128f19ee015f3cef6e
hoeeon <slhgesa@naver.com>
2017-05-15 23:49:10 +0900

    Update : Status (수정 중 )

--- lucifer/lucifer/static/js/game/User_Interface/Status_Message.js
+++ lucifer/lucifer/static/js/game/User_Interface/Status_Message.js
@@ -3,6 +3,13 @@
 var status_Message, status_Message_Group;
 var max_Index;
 var message;
+var ExpCount = 1;
+
+var status_Message;
+var status_Message2;
+var status_Message3;
+var status_Message4;
+var status_Message5;
 //-------------------------------------------------------------------------------------
 
 Status_Message = function(game, x, y, text)
@@ -29,17 +36,37 @@ Status_Message.prototype.constructor = Status_Message;
 
 function status_Message_Craete()
 {
-	status_Message_Group = Lucifer_Game.add.group();
-
-	
+	status_Message_Group = Lucifer_Game.add.group();	
 }
 
 function status_Message_Clone(text)
-{
+{	
+	
+
 	var render_Text = "경험치를 " + text + "획득하였습니다.";
 
-	var status_Message = Lucifer_Game.add.text(120, 680, render_Text, { font: "13px", fill: "#ffffff" });
-	status_Message.fixedToCamera = true;
+	switch(ExpCount){
+		case 1 :
+			status_Message = Lucifer_Game.add.text(120, 580, render_Text, { font: "13px", fill: "#ffffff" });
+			status_Message.fixedToCamera = true;
+		break;
+		case 2 :
+			status_Message2 = Lucifer_Game.add.text(120, 600, render_Text, { font: "13px", fill: "#ffffff" });
+			status_Message2.fixedToCamera = true;
+		break;
+		case 3 :
+			status_Message3 = Lucifer_Game.add.text(120, 620, render_Text, { font: "13px", fill: "#ffffff" });
+			status_Message3.fixedToCamera = true;
+		break;
+		case 4 :
+			status_Message4 = Lucifer_Game.add.text(120, 640, render_Text, { font: "13px", fill: "#ffffff" });
+			status_Message4.fixedToCamera = true;
+		break;
+		case 5 :
+			status_Message5 = Lucifer_Game.add.text(120, 660, render_Text, { font: "13px", fill: "#ffffff" });
+			status_Message5.fixedToCamera = true;
+		break;
+	}
 
 	Lucifer_Game.physics.arcade.enable(status_Message);
 
@@ -50,6 +77,8 @@ function status_Message_Clone(text)
 	Lucifer_Game.add.existing(status_Message);
 
 	status_Message_Group.add(status_Message);
+
+	ExpCount += 1;
 }
 
 function status_Message_CheckTimer(Object)
@@ -69,7 +98,7 @@ function status_Message_Update(Object)
 		if(max_Index > 0)
 		{	
 			//마지막 인덱스보다 작은 것들 새로 추가되는 것들보다 이전 것들은 위로 올린다.
-<<<<<<< HEAD
+
 			if(/*status_Message_Group.getChildIndex(message)*/ i < max_Index)
 			{
 				//var change_Message = status_Message_Group.getChildAt(i);
