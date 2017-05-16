ada8d4fda7143b3768787a00bc39096c1488033a
deadlylaid <deadlylaid@gmail.com>
2017-05-16 22:42:53 +0900

    update : modify Stage model

--- lucifer/game/stages/models/stage.py
+++ lucifer/game/stages/models/stage.py
@@ -3,10 +3,17 @@ from django.db import models
 
 class Stage(models.Model):
 
-    name = models.CharField(
+    stage = models.CharField(
+            default=1,
             max_length=20,
             )
 
-    position = models.CharField(
+    position_x = models.CharField(
+            default=875,
+            max_length=20,
+            )
+
+    position_y = models.CharField(
+            default=1637,
             max_length=20,
             )
